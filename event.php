4ecfb5bbf15f3aec3e3c6745d56cc727c7d62b88
root <root@wordpress.localdomain>
2017-06-18 16:41:47 +0900

    add event.html and event.php

---
+++ event.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Add an Event</title>
+</head>
+<body>
+
+<?php // Script 7.9 - event.php print "<p>You want to add an event called <b>{$_POST['name']} ➝ </b> which takes place on: <br>";
+
+if (isset($_POST['days']) AND is_array($_POST['days'])) {
+	foreach ($_POST['days'] as $day) {
+		print "$day<br>\n"; 
+	}
+}else{
+	print 'Please select at least one weekday for this event!';
+}
+print '</p>';
+?>
+</body> 
+</html>
+
