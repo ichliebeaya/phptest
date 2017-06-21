975b8836f44e45f85fdcfe04ce9eb201aada7c4b
ichliebeaya <ichliebeaya@gmail.com>
2017-06-21 15:40:01 +0900

    edit a lot

---
+++ includes/functions.php
@@ -0,0 +1,12 @@
+<?php //functions.php>
+
+function is_administrator($name = 'Samuel',$value='Clemens'){
+
+	if(isset($_COOKIE[$name])&&($_COOKIE[$name]==$value)){
+		return true;
+	}else{
+		return false;
+	}
+
+
+}
--- mysqli_connect.php
+++ mysqli_connect.php
@@ -10,7 +10,7 @@
 <body>
 <?php // Script 12.1 - mysql_connect.php 
 
-if ($dbc=@mysqli_connect('localhost','phptest','initial0','phptest')){
+if ($dbc=@mysqli_connect('localhost','phptest','initial0','myquotes')){
 	print '<p>Successfully connected to the database!</p>';
 	mysqli_close($dbc);
 }else{
