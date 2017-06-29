ddcd9a2e7b2f05db1f62a180ce7eac5485235bbb
ichliebeaya <yu.zhe@canon-its.co.jp>
2017-06-29 14:59:41 +0900

    add templates

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php // Script 8.4 - index.php /* This is the home page for this site.It uses templates to create the layout. */
+include('templates/header.html');
+?>
+<h2>Welcome to a J.D. Salinger Fan Club</h2>
+<p>Lorem ipsum dolor sit amet...</p>
+
+<?php
+include('templates/footer.html');
+?>
+
+
+
