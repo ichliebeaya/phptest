c7f7b487abb5144b8bcba9bd0ef42306750a31cd
ichliebeaya <yu.zhe@canon-its.co.jp>
2017-06-30 14:34:37 +0900

    add register.php

--- register.php
+++ register.php
@@ -1,56 +1,69 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/ DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/ 1999/xhtml" xml:lang="en" lang="en">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<title>Registration Form</title>
-</head>
-<body>
-<!-- Script 6.1 - register.html -->
-<div><p>Please complete this form to register:</p>
-<form action="handle_reg.php" method="post">
-<p>Email Address: <input type="text" name="email" size="30" /></p>
-<p>Password: <input type="password" name="password" size="20" /></p>
-<p>Confirm Password: <input type="password" name="confirm" size="20" /></p>
-<p>Date Of Birth:
-<select name="month">
-<option value="">Month</option>
-<option value="1">January</option>
-<option value="2">February</option>
-<option value="3">March</option>
-<option value="4">April</option>
-<option value="5">May</option>
-<option value="6">June</option>
-<option value="7">July</option>
-<option value="8">August</option>
-<option value="9">September</option>
-<option value="10">October</option>
-<option value="11">November</option>
-<option value="12">December</option>
-</select>
-<select name="day">
-<option value="">Day</option>
-<?php // Print out 31 days:
-for ($i = 1; $i <= 31; $i++) {
-	print "<option value=\"$i\">$i</option>\n";
+<?php // Script 8.9 - register.php
+
+define('TITLE', 'Register');
+include('templates/header.html');
+
+print '<h2>Registration Form</h2>
+	<p>Register so that you can take advantage of certain features like this, that, and the other thing.</p>';
+print '<style type="text/css" media="screen">
+	.error { color: red; }
+</style>';
+
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+	$problem=FALSE;
+
+	if (empty($_POST['first_name'])){
+		$problem=TRUE;
+		print '<p class="error">Please enter your last name!</p>';
+	}
+
+	if (empty($_POST['last_name'])){
+		$problem=TRUE;
+		print '<p class="error">Please enter your last name!</p>';
+	}
+	
+	if (empty($_POST['email'])) {
+		$problem = TRUE;
+		print '<p class="error">Please enter your email address!</p>';
+	}
+
+	if (empty($_POST['password1'])) {
+		$problem = TRUE;
+		print '<p class="error">Please enter a password!</p>';
+	}
+
+	if ($_POST['password1'] != $_POST['password2']){
+		$problem = TRUE;
+		print '<p class="error">Your password did not match your confirmed password!</p>';
+	}
+
+	if (!$problem){
+		print '<p>You are now registered! <br />Okay, you are not really registered but...</p>';
+		$_POST=array();
+	}else{
+		print '<p class="error">Please try again!</p>';
+	
+	}
+
 }
+
 ?>
-</select>
-<input type="text" name="year" value="YYYY" size="4" /></p>
-
-
-<p>Favorite Color:
-<select name="color">
-<option value="">Pick One</option>
-<option value="red">Red</option>
-<option value="yellow">Yellow </option>
-<option value="green">Green</option>
-<option value="blue">Blue</option> 
-</select></p>
-<p><input type="checkbox" name="terms" value="Yes" />
-I agree to the terms (whatever they may be).</p>
-<input type="submit" name="submit" value="Register" />
+<form action="register.php" method="post">
+<p>First Name: <input type="text" name="first_name" size="20" value="<?php if (isset($_POST['first_name'])){print htmlspecialchars($_POST['first_name']);}?>" /></p>
+<p>Last Name: <input type="text" name="last_name" size="20" value="<?php if (isset($_POST['last_name'])) { print htmlspecialchars($_POST['last_name']);}?>" /></p>
+<p>Password: <input type= "password" name="password1" size="20" value="<?php if (isset($_POST['password1'])) { print htmlspecialchars ($_POST['password1']); } ?>" /></p>
+<p>Confirm Password: <input type="password" name="password2" size="20" value="<?php if (isset($_POST['password2'])) { print htmlspecialchars ($_POST['password2']);} ?>" /></p>
+<p><input type="submit" name="submit" value="Register!" /></p>
 </form>
-</div>
-</body>
-</html>
+
+<?php include('templates/footer.html'); ?>
+
+
+
+
+
+
+
+
+
 
