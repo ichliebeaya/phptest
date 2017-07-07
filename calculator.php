3a0f4b56b508255f832a53fd3a9fbf61e46a6220
ichliebeaya <yu.zhe@canon-its.co.jp>
2017-07-07 13:36:14 +0900

    add calculator.php

---
+++ calculator.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Cost Calculator</title>
+</head>
+<body>
+
+<?php // Script 10.4 – calculator1.php
+
+$tax=8.75;
+
+function calculate_total($quantity,$price){
+	global $tax;
+	$total=$quantity*$price;
+	$taxrate=($tax/100)+1;
+	$total=$total*$taxrate;
+	$total=number_format($total,2);
+	return $total;
+}
+
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+	if (is_numeric($_POST['quantity']) AND is_numeric($_POST['price'])){
+		$total=calculate_total($_POST['quantity'],$_POST['price']);
+		print "<p>Your total comes to $<span style=\"font-weight: bold;\">$total</span>,including the $tax percent tax rate.</p>";
+	}else{
+		print '<p style="color: red;">Please enter a valid quantity and price!</p>';
+
+	}
+}
+?>
+
+<form action="" method="post">
+	<p>Quantity: <input type="text" name="quantity" size="3"></p>
+	<p>Price: <input type="text" name="price" size="5"></p>
+	<input type="submit" name="submit" value="Calculate!">
+</form>
+</body>
+</html>
