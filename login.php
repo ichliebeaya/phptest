c7a2359d14a2a647fd9ae7d850b486d10507c671
ichliebeaya <yu.zhe@canon-its.co.jp>
2017-07-10 13:37:46 +0900

    edit login.php

--- login.php
+++ login.php
@@ -1,34 +1,48 @@
-<?php // Script 8.8 - login.php
-define('TITLE', 'Login');
-include('templates/header.html');
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Login</title>
+</head>
+<body>
+<h1>Login</h1>
+<?php // Script 11.8 - login.php
 
-print '<h2>Login Form</h2>
-	<p>Users who are logged in can take advantage of certain features like this, that, and the other thing.</p>';
+$file='../users/users.txt';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	if ((!empty($_POST['email']))&& (!empty($_POST['password']))){
-		if ((strtolower($_POST['email']) == 'me@example.com') && ($_POST['password'] == 'testpass')){
-	session_start();
-	$_SESSION['email']=$_POST['email'];
-	$_SESSION['loggedin']=time();
-	ob_end_clean();
-	header('Location: welcome.php');
-	exit();
+if($_SERVER['REQUEST_METHOD']=='POST'){
+	$loggedin=FALSE;
+	ini_set('auto_detect_line_endings',1);
+	$fp=fopen($file,'rb');
+
+	while ($line=fgetcsv($fp,200,"\t")){
+		if(($line[0]==$_POST['username']) AND ($line[1]==md5(trim($_POST['password'])))){
+			$loggedin=TRUE;
+			break;
+
+		}
+	}
+	fclose($fp);
+	if ($loggedin){
+		print '<p>You are now logged in.</p>';
+
+	}else{
+		print '<p style="color: red;">The username and password you entered do not match those on file.</p>';
+	}
+
 }else{
-	print '<p class="text--error">The submitted email address and password do not match those on file!<br>Go back and try again.</p>';
-}
+	?>
+
+	<form action="login.php" method="post">
+		<p>Username: <input type="text" name="username" size="20" /></p>	
+		<p>Password: <input type="password" name="password" size="20" /></p>	
+		<input type="submit" name="submit" value="Login" />
+	</form>
+
+<?php } ?>
+
+</body>
+</html>
 
 
-}else{
-	print '<p class="text--error">Please make sure you enter both an email address and a password!<br>Go back and try again.</p>';
-
-}
-}else{
-	print '<form action="login.php" method="post" class="form--inline">
-	<p><label for="email">Email Address:</label><input type="email" name="email" size="20"></p>
-	<p><label for="password"> Password:</label><input type="password" name="password" size="20"></p>
-	<p><input type="submit" name="submit" value="Log In!" class="button--pill"></p>
-	</form>';
-}
-include('templates/footer.html');//Need the footer.
-?>
