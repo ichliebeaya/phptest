64b2dd32c9d4aa27b3c18870924efcb6ea2db66f
ichliebeaya <yu.zhe@canon-its.co.jp>
2017-07-11 15:37:57 +0900

    add create_table.php

---
+++ create_table.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Create a Table</title>
+</head>
+
+<body>
+<?php // Script 12.3 - create_table.php
+
+if($dbc=@mysqli_connect('localhost','phptest','initial0','phptest')){
+	$query='CREATE TABLE entries(id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,title VARCHAR(100) NOT NULL,entry TEXT NOT NULL,date_entered DATETIME NOT NULL) CHARACTER SET utf8';
+	if(@mysqli_query($dbc,$query)){
+		print '<p>The table has been created.</p>';
+	}else{
+		print '<p style="color: red;"Could not create the table because:<br>' . mysqli_error($dbc) . '.</p><p>The query being run was: ' . $query . '</p>';
+	}
+	mysqli_close($dbc);
+}else{
+
+	print '<p stype="color: red">Could not connect to the database:<br>' . mysqli_connect_error() . '.</p>';
+
+}
+?>
+</body>
+</html>
