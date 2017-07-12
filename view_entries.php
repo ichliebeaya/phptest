aa1f5fca9146c284f4ab159433eb04f378bed7ab
ichliebeaya <yu.zhe@canon-its.co.jp>
2017-07-12 17:10:37 +0900

    add delete_entry and view_entries.php

---
+++ view_entries.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>View My Blog</title>
+</head>
+
+<body>
+<h1>My Blog</h1>
+<?php 
+$dbc=mysqli_connect('localhost','phptest','initial0','phptest');
+$query='SELECT * FROM entries ORDER BY date_entered DESC';
+
+if($r=mysqli_query($dbc,$query)){
+	while($row=mysqli_fetch_array($r)){
+		print "<p><h3>{$row['title']}</h3>
+		{$row['entry']}<br>
+		<a href=\"edit_entry.php?id={$row['id']}\">Edit</a>
+		<a href=\"delete_entry.php?id={$row['id']}\">Delete</a>
+		</p><hr>\n";
+	}
+}else{
+	print '<p tyle="color: red;">Could not retrieve the data because:<br>' . mysqli_error($dbc) . '.</p><p>The query being run was: ' . $query .  '</p>';
+
+
+}
+mysqli_close($dbc);
+
+?>
+</body>
+</html>
